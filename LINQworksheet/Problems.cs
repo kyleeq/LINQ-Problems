6898eef8b97e3926a60e5e81608e0011adf8f7ae
unknown <kylee.schreibs2@yahoo.com>
2019-04-22 22:01:49 -0500

    attempt grades method

--- LINQworksheet/Problems.cs
+++ LINQworksheet/Problems.cs
@@ -10,11 +10,13 @@ namespace LINQworksheet
     {
         public List<string> words;
         public List<string> names;
+        public List<string> grades;
 
         public Problems()
         {
             words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            grades = new List<string>() {"80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55"};
         }
 
 
@@ -63,9 +65,13 @@ namespace LINQworksheet
             }
         }
 
-        public void AverageGrade()
+        public void AverageGrade(List<string> grades)
         {
-
+            var stringArray = grades.Select(g => g.Split(',')).ToList();
+            var intList = stringArray.Select(g => g.Select(a => int.Parse(a)).ToList()).ToList();
+            // remove lowest
+            // add all values in sub arrays and divide by grades.Count
+            // add averages and divide by averages.Count
         }
         public void AlphabeticOrder(string word)
         {
